34288238c787de7036d5f0fdaa322f89a8059c03
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-11-21 22:52:53 -0800

    Added test folder and tests for i2c and spi

---
+++ test/test_pn532i2c.py
@@ -0,0 +1,25 @@
+"""
+    created by Jordan Gassaway, 11/21/2019
+    Test for pn532i2c class
+"""
+from unittest import TestCase
+
+
+class TestPn532i2c(TestCase):
+    def test_begin(self):
+        self.fail()
+
+    def test_wakeup(self):
+        self.fail()
+
+    def test_writeCommand(self):
+        self.fail()
+
+    def test_getResponseLength(self):
+        self.fail()
+
+    def test_readResponse(self):
+        self.fail()
+
+    def test_readAckFrame(self):
+        self.fail()
---
+++ test/test_pn532spi.py
@@ -0,0 +1,34 @@
+"""
+    created by Jordan Gassaway, 11/21/2019
+    Test pn532spi class
+"""
+from unittest import TestCase
+
+
+class TestPn532spi(TestCase):
+    def test_write(self):
+        self.fail()
+
+    def test_read(self):
+        self.fail()
+
+    def test_begin(self):
+        self.fail()
+
+    def test_wakeup(self):
+        self.fail()
+
+    def test_writeCommand(self):
+        self.fail()
+
+    def test_readResponse(self):
+        self.fail()
+
+    def test_isReady(self):
+        self.fail()
+
+    def test_writeFrame(self):
+        self.fail()
+
+    def test_readAckFrame(self):
+        self.fail()
